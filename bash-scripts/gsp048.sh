5285952386dc948a3485403a6eca894cf25de3ae
nov05 <you@example.com>
2026-06-09 18:50:42 -0500

    .

--- bash-scripts/gsp048.sh
+++ bash-scripts/gsp048.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 ## Created by nov05, 2026-06-09
 
+## Get project id, project number, region, zone
+export PROJECT_ID=$(gcloud config get-value project)
+export PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID \
+  --format='value(projectNumber)')
+export REGION=$(gcloud compute project-info describe \
+  --format="value(commonInstanceMetadata.items[google-compute-default-region])")
+export ZONE=$(gcloud compute project-info describe \
+  --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+gcloud config set project $PROJECT_ID  
+gcloud config set compute/region $REGION
+gcloud config set compute/zone $ZONE
+echo
+echo "🔹  User: $USER"
+echo "🔹  Project ID: $PROJECT_ID"
+echo "🔹  Region: $REGION"
+echo "🔹  Zone: $ZONE"
+echo
+gcloud auth list
+
 cat << 'EOF'
 
 ========================================================
