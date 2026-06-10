aeb6f31e9735718c30d314aef31a747c86da6544
nov05 <you@example.com>
2026-06-10 11:37:51 -0500

    .

--- bash-scripts/gsp081.sh
+++ bash-scripts/gsp081.sh
@@ -28,6 +28,12 @@ Task 2. Deploy the function
 
 EOF
 
+gcloud services enable run.googleapis.com \
+  --project=$PROJECT_ID
+until gcloud services list --enabled \
+  --project=$PROJECT_ID | grep -q run.googleapis.com
+do sleep 5; done
+
 gcloud run deploy gcfunction \
     --source=. \
     --region=REGION \
