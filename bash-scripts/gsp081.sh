a22584218c6c65f4796b2cac706744dc683f04c5
nov05 <you@example.com>
2026-06-10 15:27:32 -0500

    .

--- bash-scripts/gsp081.sh
+++ bash-scripts/gsp081.sh
@@ -30,26 +30,64 @@ Task 2. Deploy the function
 
 EOF
 
+echo -e "\n👉  Enableing services...\n"
 gcloud services enable run.googleapis.com \
-    artifactregistry.googleapis.com \
-    cloudbuild.googleapis.com \
-    cloudfunctions.googleapis.com \
-    --project=$PROJECT_ID
-until gcloud services list --enabled \
-    --project=$PROJECT_ID | grep -q run.googleapis.com
-do sleep 5; done
+  artifactregistry.googleapis.com \
+  cloudbuild.googleapis.com \
+  --project=$PROJECT_ID
+# until enabled=$(gcloud services list --enabled --project=$PROJECT_ID); \
+#   echo "$enabled" | grep -q run.googleapis.com && \
+#   echo "$enabled" | grep -q artifactregistry.googleapis.com && \
+#   echo "$enabled" | grep -q cloudbuild.googleapis.com
+# do sleep 5; done
+sleep 10
 
-gcloud artifacts repositories create cloud-run-source-deploy \
-  --repository-format=docker \
-  --location=$REGION \
-  --description="GSP081 Cloud Run" \
-  || true
+mkdir myfunc && cd myfunc
+cat > index.js << 'EOF'
+const functions = require('@google-cloud/functions-framework');
+functions.http('helloHttp', (req, res) => {
+  res.set('Content-Type', 'text/plain');
+  res.send(`Hello ${req.query.name || req.body.name || 'World'}!`);
+});
+EOF
+cat > package.json << 'EOF'
+{
+  "dependencies": {
+    "@google-cloud/functions-framework": "^3.0.0"
+  }
+}
+EOF
+cd ..
 
-gcloud run deploy gcfunction \
-    --source=. \
-    --region=REGION \
-    --allow-unauthenticated \
-    --max-instances=5
+echo -e "\n👉  Deploying Cloud Run function 'gcfunction'...\n"
+gcloud functions deploy gcfunction \
+  --gen2 \
+  --runtime=nodejs22 \
+  --region=$REGION \
+  --source=./myfunc \
+  --entry-point=helloHttp \
+  --trigger-http \
+  --allow-unauthenticated \
+  --max-instances=5
+
+# gcloud run services update gcfunction \
+#   --region=$REGION \
+#   --max-instances=8
+
+echo -e "\n👉  Check revisions:\n"
+## Sample return: gcfunction-00001-far
+gcloud run revisions list \
+  --service=gcfunction \
+  --region=$REGION \
+  --sort-by="~metadata.creationTimestamp" \
+  # --limit=1 \
+  --format="value(name)"
+
+echo -e "\n👉  Check Cloud Run function 'gcfunction' configurations:\n"
+gcloud run services describe gcfunction \
+  --region=$REGION \
+  --format="yaml(spec.template)"
+
 
 cat << 'EOF'
 
@@ -59,12 +97,12 @@ Task 3. Test the function
 
 EOF
 
-gcloud run services proxy gcfunction --region=REGION &
-curl -X POST \
-  -H "Content-Type: application/json" \
-  -d '{"message":"Hello World!"}' \
-  http://localhost:8080
-
+curl -X POST "https://${REGION}-${PROJECT_ID}.cloudfunctions.net/gcfunction" \
+-H "Authorization: bearer $(gcloud auth print-identity-token)" \
+-H "Content-Type: application/json" \
+-d '{
+  "name": "Developer"
+}'
 
 cat << 'EOF'
 
@@ -76,7 +114,7 @@ EOF
 
 gcloud logging read \
 'resource.type="cloud_run_revision" AND resource.labels.service_name="gcfunction"' \
-    --limit=50 \
+    --limit=20 \
     --format="table(timestamp,severity,textPayload)"
 
 
@@ -91,6 +129,7 @@ Task 5. Test your understanding
 
 2. Which type of trigger is used while creating Cloud Run functions in the lab?
   HTTPS
+
 EOF
 
 echo -e "\n✅  All done\n"
